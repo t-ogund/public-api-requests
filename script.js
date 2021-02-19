ea8baa9b9801c51db45f4c73b6dc5913e8285e9d
Toye Ogundepo <ogundepo@gmail.com>
2021-02-18 19:55:57 -0600

    create and populate modal

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const modalContainer = document.querySelector(".modal-container");
 fetch("https://randomuser.me/api/?&results=12")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    const employees = data.results.map((employee) => {
+    // console.log(data);
+    const employees = data.results.map((employee, index) => {
       gallery.insertAdjacentHTML(
         "beforeend",
         `<div class="card">
@@ -21,41 +21,91 @@ fetch("https://randomuser.me/api/?&results=12")
     </div>
     `
       );
+
+      
+
     });
-
-    gallery.addEventListener("click", (e) => {
-      const name = document.querySelectorAll(".name");
-
-      if (e.target.className.includes("card")) {
-          console.log(e.target.parentElement)
-
     
-          
+    const card = document.querySelectorAll(".card");
+    console.log(card)
+    for (let i = 0; i < card.length; i++) {
+      card[i].addEventListener("click", () => {
+        console.log(i)
+
+        console.log(data.results[i])
         body.insertAdjacentHTML(
-          "beforeend",
-          `
-                <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
-                        <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
-                    </div>
-                </div>
-            </div>
+                "beforeend",
                 `
-        );
-        const modalCloseBtn = document.querySelector("#modal-close-btn");
+                      <div class="modal-container">
+                      <div class="modal">
+                          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                          <div class="modal-info-container">
+                              <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
+                              <h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                              <p class="modal-text">${data.results[i].email}</p>
+                              <p class="modal-text cap">${data.results[i].location.city}</p>
+                              <hr>
+                              <p class="modal-text">(555) 555-5555</p>
+                              <p class="modal-text">
+                              ${data.results[i].location.street.number} 
+                              ${data.results[i].location.street.name}, 
+                              ${data.results[i].location.city},
+                              ${data.results[i].location.state} 
+                              ${data.results[i].location.postcode}
+                              </p>
+                              <p class="modal-text">Birthday: 10/21/2015</p>
+                          </div>
+                      </div>
+                  </div>
+                      `
+              );
+              const modalCloseBtn = document.querySelector("#modal-close-btn");
       modalCloseBtn.addEventListener("click", (e) => {
         e.target.parentElement.parentElement.parentElement.remove();
-      });
-      }
+      })
+      })
+
+       
+
+    }
+
+
       
-    });
+      
+
+    //   if (e.target.className.includes("card")) {  
+    //     body.insertAdjacentHTML(
+    //       "beforeend",
+    //       `
+    //             <div class="modal-container">
+    //             <div class="modal">
+    //                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    //                 <div class="modal-info-container">
+    //                     <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+    //                     <h3 id="name" class="modal-name cap">name</h3>
+    //                     <p class="modal-text">email</p>
+    //                     <p class="modal-text cap">city</p>
+    //                     <hr>
+    //                     <p class="modal-text">(555) 555-5555</p>
+    //                     <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+    //                     <p class="modal-text">Birthday: 10/21/2015</p>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //             `
+    //     );
+    
+
+    //     const modalCloseBtn = document.querySelector("#modal-close-btn");
+    //   modalCloseBtn.addEventListener("click", (e) => {
+    //     e.target.parentElement.parentElement.parentElement.remove();
+    //   });
+    //   }
+      
+    // });
+
+    
   });
+
+
+  
