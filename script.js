3ab57a2d14d6b5d219cfe5c41943343f00a05dae
Toye Ogundepo <ogundepo@gmail.com>
2021-02-19 10:05:17 -0600

    add filter feature

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const gallery = document.querySelector("#gallery");
 const body = document.querySelector("body");
 const modalContainer = document.querySelector(".modal-container");
+const searchContainer = document.querySelector(".search-container");
+
+searchContainer.insertAdjacentHTML("beforeend", `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`)
+
 
 fetch("https://randomuser.me/api/?nat=us&results=12")
   .then((response) => response.json())
   .then((data) => {
-    // console.log(data);
     const employees = data.results.map((employee, index) => {
       gallery.insertAdjacentHTML(
         "beforeend",
@@ -21,26 +27,16 @@ fetch("https://randomuser.me/api/?nat=us&results=12")
     </div>
     `
       );
-
-      
-
     });
 
-    
-    
     const card = document.querySelectorAll(".card");
-    console.log(card)
     for (let i = 0; i < card.length; i++) {
       card[i].addEventListener("click", () => {
-        console.log(i)
-
-        console.log(data.results[i])
 
         function cellFormatter(x) {
           const areaCode = x.slice(0, 5)
           const number = (x.slice(6))
           const cellNumber = `${areaCode} ${number}`
-          console.log(cellNumber)
           return cellNumber
         }
 
@@ -49,10 +45,8 @@ fetch("https://randomuser.me/api/?nat=us&results=12")
           const day = x.slice(8, 10)
           const year = x.slice(0, 4)
           const formattedDOB = `${month}/${day}/${year}`
-          console.log(formattedDOB)
           return formattedDOB
         }
-        
         
         body.insertAdjacentHTML(
                 "beforeend",
@@ -77,55 +71,51 @@ fetch("https://randomuser.me/api/?nat=us&results=12")
                               <p class="modal-text">Birthday: ${dobFormatter(data.results[i].dob.date)}</p>
                           </div>
                       </div>
+
+                      // IMPORTANT: Below is only for exceeds tasks 
+                      <div class="modal-btn-container">
+                          <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                          <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                      </div>
                   </div>
                       `
               );
               const modalCloseBtn = document.querySelector("#modal-close-btn");
-      modalCloseBtn.addEventListener("click", (e) => {
-        e.target.parentElement.parentElement.parentElement.remove();
-      })
-      })
+              // const next = document.querySelector("#modal-next");
+              // const prev = document.querySelector("#modal-prev");
 
-       
 
+              // const modalToggle = document.querySelector(".modal-btn-container");
+              // modalToggle.addEventListener("click", () => {
+
+              // })
+
+              modalCloseBtn.addEventListener("click", (e) => {
+                e.target.parentElement.parentElement.parentElement.remove();
+              })
+
+              
+      })
     }
 
-
-      
-      
-
-    //   if (e.target.className.includes("card")) {  
-    //     body.insertAdjacentHTML(
-    //       "beforeend",
-    //       `
-    //             <div class="modal-container">
-    //             <div class="modal">
-    //                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    //                 <div class="modal-info-container">
-    //                     <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-    //                     <h3 id="name" class="modal-name cap">name</h3>
-    //                     <p class="modal-text">email</p>
-    //                     <p class="modal-text cap">city</p>
-    //                     <hr>
-    //                     <p class="modal-text">(555) 555-5555</p>
-    //                     <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-    //                     <p class="modal-text">Birthday: 10/21/2015</p>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //             `
-    //     );
-    
-
-    //     const modalCloseBtn = document.querySelector("#modal-close-btn");
-    //   modalCloseBtn.addEventListener("click", (e) => {
-    //     e.target.parentElement.parentElement.parentElement.remove();
-    //   });
-    //   }
-      
-    // });
-
-    
+    const searchInput = document.querySelector(".search-input");
+    const searchSubmit = document.querySelector(".search-submit");
+    const cardName = document.querySelectorAll(".card-name");
+    const arrayOfNames = Array.from(cardName);
+    console.log(arrayOfNames)
+    searchInput.addEventListener("keyup", (e) => {
+      console.log(cardName)
+      arrayOfNames.filter(name => {
+        if (name.textContent.toLowerCase().includes(e.target.value) === true) {
+          name.parentElement.parentElement.style.display = "flex";
+        } else {
+          name.parentElement.parentElement.style.display = "none";
+        }
+      })
+    })
+    searchSubmit.addEventListener("click", () => {
+      console.log(searchInput.value)
+    })
   });
 
 
